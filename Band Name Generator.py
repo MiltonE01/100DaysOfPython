2f3fff8c4efd5ce4a619b24092b3ff5637074733
MiltonE01 <102384814+MiltonE01@users.noreply.github.com>
2026-06-17 22:37:44 -0400

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print ("Your band name could be " + city + " " + pet)
+
+
+## This is a simple band name generator that takes the user's input for their city and pet's name to create a potential band name. The user is prompted to enter their city and pet's name, and then the program combines these inputs to suggest a band name.
+
