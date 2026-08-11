68efdb7189f0eac7dad15ffb16f57d9c6107608d
MiltonE01 <102384814+MiltonE01@users.noreply.github.com>
2026-08-11 13:55:01 -0400

    Add password generator functionality

--- (Day 5)Python Loops.py
+++ (Day 5)Python Loops.py
@@ -1,1 +1,27 @@
-..
+
+import random
+
+print("Welcome to the Password Generator!")
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+password = ""
+
+
+for char in range(1, nr_letters + 1):
+    password += random.choice(letters)
+
+for char in range(1, nr_symbols + 1):
+    password += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+    password += random.choice(numbers)
+
+print(f"Your generated password is: {password}")
