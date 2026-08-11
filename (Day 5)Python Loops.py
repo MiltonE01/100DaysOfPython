3a5fb9a06fccab1cbf852898008c13f81c574b00
MiltonE01 <102384814+MiltonE01@users.noreply.github.com>
2026-08-11 14:03:21 -0400

    Improve password generator logic
    Refactored password generation to use a list and shuffle the result.

--- (Day 5)Python Loops.py
+++ (Day 5)Python Loops.py
@@ -1,4 +1,3 @@
-
 import random
 
 print("Welcome to the Password Generator!")
@@ -12,16 +11,21 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 
-password = ""
+password_list = []
 
 
 for char in range(1, nr_letters + 1):
-    password += random.choice(letters)
+    password_list.append(random.choice(letters))
 
 for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
+    password_list.append(random.choice(symbols))
 
 for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = "".join(password_list)
 
 print(f"Your generated password is: {password}")
+
